89d97717d8f73251d0b5465be8d6dd70de2a3285
AADIL LAPTOPS <shakeel.nazir918@gmail.com>
2022-11-28 12:08:45 +0100

    1st pull added as comment

--- python_udemy_basics.py
+++ python_udemy_basics.py
@@ -4,3 +4,6 @@ def print_big(letter):
     alphabet = {'A':[1,2,4,3,3],'B':[5,3,5,3,5],'C':[4,9,9,9,4],'D':[5,3,3,3,5],'E':[4,9,4,9,4]}
     for pattern in alphabet[letter.upper()]:
         print(patterns[pattern])
+
+
+#Ist pull on Github
