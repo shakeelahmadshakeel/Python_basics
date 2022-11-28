120d51c0f8bfee14aef577e5b2e026812185bd89
AADIL LAPTOPS <shakeel.nazir918@gmail.com>
2022-11-28 12:16:46 +0100

    1st pull added as comment code added to view full alphabets

--- python_udemy_basics.py
+++ python_udemy_basics.py
@@ -7,3 +7,10 @@ def print_big(letter):
 
 
 #Ist pull on Github
+patterns = {1:'  *  ',2:' * * ',3:'*   *',4:'*****',5:'**** ',6:'   * ',7:' *   ',8:'*   * ',9:'*    '}
+alphabet = {'A':[1,2,4,3,3],'B':[5,3,5,3,5],'C':[4,9,9,9,4],'D':[5,3,3,3,5],'E':[4,9,4,9,4]}
+for k in alphabet:
+    print(k)
+    for i in alphabet[k]:
+        print(patterns[i])
+    print(alphabet[k])
